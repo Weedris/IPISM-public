41e089943e9416e8547852fdc9d64363a9b1d1ba
Weedris <48325911+Weedris@users.noreply.github.com>
2021-05-12 11:52:51 +0200

    Delete setup.py

--- Interface/setup.py
+++
@@ -1,9 +0,0 @@
-from cx_Freeze import setup, Executable
-
-# On appelle la fonction setup
-setup(
-    name = "test",
-    version = "1.0",
-    description = "chose qui mesure les molécules",
-    executables = [Executable("main.py")],
-)
